2d47dc0f3a53db008ebf8f3c7f0eefd88c2f1085
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2024-02-05 10:50:33 +0000

    chore(added): import restcontroller

--- spring/exception/GlobalExceptionHandler.java
+++ spring/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import com.task.manager.modules.validation.Exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
